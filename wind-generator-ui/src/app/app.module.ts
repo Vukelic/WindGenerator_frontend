549cda944463b682a3d78d138acb9defc15d74fb
bilja.vukelic18 <bilja.vukelic18@gmail.com>
2021-11-28 14:54:17 +0100

    Added basic components,services,dto models..

--- wind-generator-ui/src/app/app.module.ts
+++ wind-generator-ui/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { MarkerInfoBoxComponent } from './components/marker-info-box/marker-info-box.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    MarkerInfoBoxComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
---
+++ wind-generator-ui/src/app/components/marker-info-box/marker-info-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marker-info-box',
+  templateUrl: './marker-info-box.component.html',
+  styleUrls: ['./marker-info-box.component.scss']
+})
+export class MarkerInfoBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
