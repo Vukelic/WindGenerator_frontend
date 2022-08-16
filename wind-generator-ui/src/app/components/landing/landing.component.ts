d2978042360e2015ccff21d645c470313645ac5a
bilja.vukelic18 <bilja.vukelic18@gmail.com>
2022-08-16 16:11:10 +0200

    added mockup of wind turbines

--- wind-generator-ui/src/app/components/landing/landing.component.ts
+++ wind-generator-ui/src/app/components/landing/landing.component.ts
@@ -9,9 +9,98 @@ export class LandingComponent implements OnInit {
 
   path1:any = "/assets/pic1.JPG";
   path2:any = "/assets/pic2.JPG";
+
+  listOfDeviceType: any = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.getTypes();
+  }
+
+  edit(){
+
+  }
+
+  
+
+  getTypes() {
+    this.listOfDeviceType = [   
+      {
+       Name: "Type 1", 
+       Turbines: "Radial",
+       PowerOfTurbines: "3kW",
+       HeightOfWing: "3.2M",
+       WidthOfWing: '1.8M',
+       Weight: "300kg",
+       MaxPowerTurbine: "14m/s",
+       MaxSpeedTurbine: "50m/s",
+       GeneratorPower:"1KW",
+       Guarantee:"5 Years",   
+       ImageUrl: "/assets/wt1.JPG"    
+      },
+      {
+        Name: "Type 2", 
+        Turbines: "Radial",
+        PowerOfTurbines: "3kW",
+        HeightOfWing: "3.2M",
+        WidthOfWing: '1.8M',
+        Weight: "300kg",
+        MaxPowerTurbine: "14m/s",
+        MaxSpeedTurbine: "50m/s",
+        GeneratorPower:"1KW",
+        Guarantee:"5 Years",   
+        ImageUrl: "/assets/wt2.JPG"   
+       },   
+      {
+        Name: "Type 3", 
+        Turbines: "Radial",
+        PowerOfTurbines: "3kW",
+        HeightOfWing: "3.2M",
+        WidthOfWing: '1.8M',
+        Weight: "300kg",
+        MaxPowerTurbine: "14m/s",
+        MaxSpeedTurbine: "50m/s",
+        GeneratorPower:"1KW",
+        Guarantee:"5 Years",  
+        ImageUrl: "/assets/wt3.JPG"       
+       },
+       {
+        Name: "Type 4", 
+        Turbines: "Radial",
+        PowerOfTurbines: "3kW",
+        HeightOfWing: "3.2M",
+        WidthOfWing: '1.8M',
+        Weight: "300kg",
+        MaxPowerTurbine: "14m/s",
+        MaxSpeedTurbine: "50m/s",
+        GeneratorPower:"1KW",
+        Guarantee:"5 Years",  
+        ImageUrl: "/assets/wt4.JPG"       
+       },
+  ]
+    // var dtoEntityIds = new DtoEntityIds();
+    // dtoEntityIds.Ids = this.auth.userPermitions.DashboardLive.View.EntityIds;
+    // console.warn(this.auth.userPermitions);
+    // if (this.auth.userPermitions.role == "SuperAdmin" || this.auth.userPermitions.DashboardLive.View.ClaimForAllEntities) {
+    //   console.warn(this.auth.userPermitions);
+    //   dtoEntityIds.ReturnAll = true;
+    // }
+
+    // this.dashboardService.GetDashboardsByIds(dtoEntityIds).subscribe((dashboardResp: DtoDashboardListResponse) => {
+    //   console.log(`%cdashboardResp`, 'color: lime');
+    //   console.log(dashboardResp);
+    //   if (dashboardResp.Success) {
+    //     if (dashboardResp.Value == null || dashboardResp.Value.length == 0) {
+    //       //There is not active dashboards 
+    //     }
+
+    //   } else {
+    //     //There is not active dashboards and error
+
+    //   }
+
+    //   this.listOfDashboards = dashboardResp.Value;
+    // });
   }
 
 }
